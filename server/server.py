a83791eefff23c1693001ffb697a3c5b8a961f42
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-06-30 12:02:12 +0800

    define basic routes on server

--- server/server.py
+++ server/server.py
@@ -8,11 +8,32 @@
 API endpoints process transactions with database
 
 '''
+
+##  SERVER CONFIGURATION
+PORT = 8000
+
+
+
+# import dependencies
 from flask import Flask
+
+
 
 app = Flask(__name__)
 
+# Home
 @app.route("/")
 def home():
     return "<h1>ESPesos Exchange Homepage</h1>"
 
+# Transaction
+@app.route("/transact/")
+def transact():
+    return "<h1>Transact ESPesos</h1>"
+
+# Account Info
+@app.route("/account/")
+def account():
+    return "<h1>ESPesos Account Information</h1>"
+
+app.run(port=PORT)
