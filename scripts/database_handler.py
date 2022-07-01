c273e543eff19ceab773bb8904a9f906229eaf29
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-01 22:07:16 +0800

    define contextual db queries (without data validation yet)

--- scripts/database_handler.py
+++ scripts/database_handler.py
@@ -6,33 +6,8 @@ DATABASE FUNCTIONS
 PATH_TO_DATA = "../data"
 EXCHANGE_DATABASE = "exchange.db"
 
-
 #import dependencies
 import sqlite3 as sq
-
-##HIGHER LEVEL USE FUNCTIONS
-def add_account():
-    pass
-
-def remove_account():
-    pass
-
-def get_account():
-    pass
-
-def search_account():
-    pass
-
-def transact():
-    pass
-
-def find_transaction():
-    pass
-
-def find_transaction_raw():
-    pass
-
-
 class ExchangeDatabase():
     def __init__(self):
         self._db = None
@@ -69,3 +44,34 @@ class ExchangeDatabase():
             print(e)
         finally:
             return results   
+
+    def add_account(self, values):
+        #check all fields have been input
+        # if not values.has_key('token') or not values.has_key('name') or not values.has_key('fingerprint') or not values.has_key('balance'):
+        #     return False
+        self.query("INSERT INTO accounts VALUES ('{token}', '{name}', '{fingerprint}', {balance})".format(
+            token=values['token'], name=values['name'], fingerprint=values['fingerprint'], balance=values['balance']))
+
+    def remove_account(self, token):
+        return self.query("DELETE * from accounts WHERE token='{token}'".format(token=token))
+
+    def get_account(self, token):
+        return self.query("SELECT * from accounts WHERE token='{token}'".format(token=token))
+
+    def search_account(self, name):
+        return self.query("SELECT * from accounts WHERE name='{name}'".format(name=name))
+
+    def transact(self, values):
+        pass
+
+    def get_transaction(self, transaction_id):
+        return self.query("SELECT * from transactions WHERE transaction_id='{transaction_id}'".format(transaction_id=transaction_id))
+
+    def find_transaction(self, sender_token, recipient_token):
+        return self.query("SELECT * from transactions WHERE sender_token='{sender_token}' & recipient_token='{recipient_token}'".format(sender_token=sender_token, recipient_token=recipient_token))
+
+## test zone
+ex = ExchangeDatabase()
+ex.add_account({'token':'A1', 'fingerprint':'A', 'name':"Johnny", "balance":10.0})
+print(ex.get_account("A1"))
+print(ex.query("SELECT * from transactions"))
